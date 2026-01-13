00d76b155e3481b95dc9552294f27cca4a614188
Sean Lees <sean@Seans-MacBook-Air.local>
2026-01-13 13:51:24 +0000

    fix: ccsl version to 0.1.3

--- Formula/ccsl.rb
+++ Formula/ccsl.rb
@@ -6,7 +6,7 @@
 class Ccsl < Formula
   desc "Powerline-style status line for Claude Code"
   homepage "https://github.com/seanmozeik/ccsl"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
