7894af0d8be6f59f4d99d4f08d3f3b31d4308e2b
Sean Lees <sean@Seans-MacBook-Air.local>
2026-02-16 02:59:51 +0000

    s3up 0.3.2

--- Formula/s3up.rb
+++ Formula/s3up.rb
@@ -1,12 +1,12 @@
 class S3up < Formula
   desc "Fast CLI tool for uploading files to S3-compatible storage"
   homepage "https://github.com/seanmozeik/s3up"
-  version "0.3.1"
+  version "0.3.2"
   license "MIT"
 
   # URL to bundled source (single JS file)
   url "https://github.com/seanmozeik/s3up/releases/download/v#{version}/s3up-#{version}.tar.gz"
-  sha256 "3b448e8f2f4b1c6ae032869651d7eab0ac6faedfd94b31037c22332b84cec735"
+  sha256 "570a68ab5348d340165d4eb97d6e2804e79acaaffcd071d6b2a25c7544e72662"
 
   depends_on "oven-sh/bun/bun"
 
