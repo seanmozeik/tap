ff3a25f8fd009207c05c360c6a96b4eaebf02f00
Sean Lees <sean@Seans-MacBook-Air.local>
2026-02-25 13:39:46 +0000

    md 0.3.5

--- Formula/md.rb
+++ Formula/md.rb
@@ -1,12 +1,12 @@
 class Md < Formula
   desc "Beautiful terminal markdown viewer"
   homepage "https://github.com/seanmozeik/markdown-display"
-  version "0.3.4"
+  version "0.3.5"
   license "MIT"
 
   # URL to bundled source (single JS file)
   url "https://github.com/seanmozeik/markdown-display/releases/download/v#{version}/md-#{version}.tar.gz"
-  sha256 "81dbfa737c06d21e679b7dc71f5488c37e932db8bf7d3a8626e435e86f337e5a"
+  sha256 "48ef957fe9fb227953b48d7a12e11e1851f06b63613ab5483c196eed3ab1cbb7"
 
   depends_on "oven-sh/bun/bun"
 
