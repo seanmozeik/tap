447d54ecbea827bd6b48486378ae39a1e263f458
seanmozeik <sean@mozeik.com>
2026-02-26 19:35:49 +0000

    aic 0.3.7

--- Formula/aic.rb
+++ Formula/aic.rb
@@ -1,13 +1,13 @@
 class Aic < Formula
   desc "AI-powered commit message generator using conventional commit format"
   homepage "https://github.com/seanmozeik/AICommit"
-  version "0.3.6"
+  version "0.3.7"
   license "MIT"
 
   url "https://github.com/seanmozeik/AICommit/releases/download/v#{version}/aic-#{version}.tar.gz"
-  sha256 "dadc0b339bae9af6497a7c92c18f8cf1cc7b7b58d545485ee32a92aa8e9d4494"
+  sha256 "07a1ee7904b6eb2fb1cd20949f6aa229f490efdad91c3d7aee2e0c55df5496ec"
 
-  depends_on "oven-sh/bun/bun"
+  depends_on "bun"
 
   on_linux do
     depends_on "libsecret"
