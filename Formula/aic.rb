e51c3b2348eedd9f2af9a42c511fe8fcf388f475
seanmozeik <sean@mozeik.com>
2026-02-26 19:47:06 +0000

    aic 0.3.8

--- Formula/aic.rb
+++ Formula/aic.rb
@@ -1,11 +1,11 @@
 class Aic < Formula
   desc "AI-powered commit message generator using conventional commit format"
   homepage "https://github.com/seanmozeik/AICommit"
-  version "0.3.7"
+  version "0.3.8"
   license "MIT"
 
   url "https://github.com/seanmozeik/AICommit/releases/download/v#{version}/aic-#{version}.tar.gz"
-  sha256 "07a1ee7904b6eb2fb1cd20949f6aa229f490efdad91c3d7aee2e0c55df5496ec"
+  sha256 "866f51b179ef8402dfdaf34322b9539cbf371018ac70543d6459c7f1ac59e13a"
 
   depends_on "bun"
 
@@ -17,7 +17,7 @@ class Aic < Formula
     libexec.install Dir["*"]
     (bin/"aic").write <<~EOS
       #!/bin/bash
-      exec bun "#{libexec}/index.js" "$@"
+      exec bun "#{libexec}/aic.js" "$@"
     EOS
   end
 
