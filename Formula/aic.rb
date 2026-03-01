0c96c57009480056101f5ae88142089aea506f65
seanmozeik <sean@mozeik.com>
2026-03-01 15:03:13 +0000

    aic 0.3.9

--- Formula/aic.rb
+++ Formula/aic.rb
@@ -1,11 +1,11 @@
 class Aic < Formula
   desc "AI-powered commit message generator using conventional commit format"
   homepage "https://github.com/seanmozeik/AICommit"
-  version "0.3.8"
+  version "0.3.9"
   license "MIT"
 
   url "https://github.com/seanmozeik/AICommit/releases/download/v#{version}/aic-#{version}.tar.gz"
-  sha256 "866f51b179ef8402dfdaf34322b9539cbf371018ac70543d6459c7f1ac59e13a"
+  sha256 "720740c650b4d72b5155f5404a04fe70831bb5885e3a3d54c310b30470a49f43"
 
   depends_on "bun"
 
