73b0d36c56938dbd27f71f826fbffb0b071d75ae
seanmozeik <sean@mozeik.com>
2026-03-14 20:49:14 +0000

    claudewatch 0.2.1

--- Formula/claudewatch.rb
+++ Formula/claudewatch.rb
@@ -1,12 +1,12 @@
 class Claudewatch < Formula
   desc "Zero-argument CLI that continuously polls for Git file changes and renders syntax-highlighted unified diffs in the terminal"
   homepage "https://github.com/seanmozeik/claudewatch"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   # URL to bundled source (single JS file)
   url "https://github.com/seanmozeik/claudewatch/releases/download/v#{version}/claudewatch-#{version}.tar.gz"
-  sha256 "d358e17ac307bd9263f1819ae414ee0c8d585805ba5f41d0fd154e39dc00a950"
+  sha256 "665ba5251f7d6213f28c603775557358f64a2704716b3709a74ca64879d8e020"
 
   depends_on "oven-sh/bun/bun"
 
